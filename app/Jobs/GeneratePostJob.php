375092935d52f4430857036f3cc2b3006585cc04
Mimi-elmo <elmoaouine.mariam@gmail.com>
2026-07-08 11:26:11 +0100

    feat(queue): dispatch post generation job

--- app/Jobs/GeneratePostJob.php
+++ app/Jobs/GeneratePostJob.php
@@ -1,27 +1,21 @@
 <?php
 
 namespace App\Jobs;
-
+use App\Models\RawContent;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+
 
 class GeneratePostJob implements ShouldQueue
 {
     use Queueable;
 
-    /**
-     * Create a new job instance.
-     */
-    public function __construct()
-    {
-        //
-    }
+    public function __construct(
+        public RawContent $rawContent
+    ) {}
 
-    /**
-     * Execute the job.
-     */
     public function handle(): void
     {
-        //
+        logger('Generating post for RawContent ID: '.$this->rawContent->id);
     }
 }
